27b865714a1ad88cb754fd17166ce0aa24bf9e48
farhad6366 <0325714958@student.wub.edu.bd>
2026-07-23 19:46:17 +0600

    odd number

---
+++ odd.js
@@ -0,0 +1,13 @@
+function isOdd(farhad){
+      if(farhad % 3===1){
+        return true
+      }
+      else{
+
+        return false;
+      }
+}
+const result = isOdd(67)
+const result1= isOdd(44)
+const result2= isOdd(99)
+console.log(result,result1,result2)
