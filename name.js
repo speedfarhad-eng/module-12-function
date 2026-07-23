12581b10367a8cc7d749c57b7d10b2153b35621d
farhad6366 <0325714958@student.wub.edu.bd>
2026-07-23 21:50:42 +0600

    name add

---
+++ name.js
@@ -0,0 +1,14 @@
+function chowdurify (name){
+    const fullName = name +' '+ "chowdury"
+    return fullName
+}
+const siamchow= chowdurify('siam')
+const farhadchow= chowdurify('farhad')
+// console.log(siamchow)
+// console.log(farhadchow)
+function fullName(firstName,lastName){
+    const fullName = firstName+' '+lastName
+    return fullName
+}
+const mohabbat= fullName('mohabbat','ali')
+// console.log(mohabbat);
