57ee11651e8e2da15b48068b91a9c2304ecbbf86
farhad6366 <0325714958@student.wub.edu.bd>
2026-07-23 19:41:02 +0600

    evan number

---
+++ evan.js
@@ -0,0 +1,14 @@
+function isEvan(number){
+console.log (number%2)
+if(number%2 ===0){
+    return true;
+}else{
+    return false;
+}
+}
+const result= isEvan(49)
+const result2=isEvan(34)
+const result3= isEvan(68)
+console.log(result)
+console.log(result2)
+console.log(result3)
